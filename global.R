8919a08ce333dc45b0e6363778fbed474a1adafd
cwainright-nps <charles_wainright@nps.gov>
2025-04-30 13:39:57 -0400

    [bugfix] keep parameter2 value instead of resetting to NA when park changes

--- global.R
+++ global.R
@@ -135,7 +135,7 @@ paramChooser2<-function(input, output, session, data, park, site, chosen){
    })
   
   observe(
-    updateSelectizeInput(session, inputId="ParamIn2",selected=NA, 
+    updateSelectizeInput(session, inputId="ParamIn2",selected=chosen(), 
                          choices=c("Choose a Parameter"="", as.list(sort(PChoices()))))
   )
   
